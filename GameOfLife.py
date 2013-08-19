31bf6f8c2d2b8b0d3798b616b14f2bdfb201a320
Juan Pablo <pabloitto@gmail.com>
2013-08-19 16:51:38 -0700

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -12,18 +12,12 @@ import random
     alive or dead =)
 """
 
-class GameOfLife(object):
+class GameOfLife():
 
-	__ALIVE = True #represent a alive cell
-	__DEAD = False #represent a dead cell
-
-	"""Private functions"""
-
-	#init the size of panel
 	def __init__(self):
-		#board 40 x 40
 		self.width = 40
 		self.heigth = 40
+		self.board = self.__get_board()
 
 	#return the new state of node(depends of the neighbors alive)
 	#1) Any live cell with fewer than two live neighbors dies,
@@ -35,70 +29,63 @@ class GameOfLife(object):
 	#4) Any dead cell with exactly three live neighbors becomes a live cell,
 	#   as if by reproduction
 	def __change_state(self , current_state , live_cells):
-		if live_cells == 3 or (live_cells == 2 and current_state == self.__ALIVE):
-			return self.__ALIVE
-		return self.__DEAD
+		return live_cells == 3 or (live_cells == 2 and current_state)
+			
 
-	
 	#Every cell interacts with its eight neighbors, which are 
 	#the cells that are horizontally, vertically, or diagonally 
 	#adjacent. At each step in time(generation).
-	def __get_alive_neighbors(self , x , y , board):
-
+	def __get_alive_neighbors(self , x , y):
 		alive_neighbors = 0
 
-		current_position = board[x][y] # get current cell
+		current_position = self.board[x][y] # get current cell
 
-		size = len(board) - 1
-		
+		size = len(self.board) - 1
+
 		#left - right and top - down
-		
 		if (x - 1) > 0:
-			if board[x - 1][y] == self.__ALIVE:
+			if self.board[x - 1][y]:
 				alive_neighbors = alive_neighbors + 1
 
 		if (x + 1) <= size:
-			if board[x + 1][y] == self.__ALIVE:
+			if self.board[x + 1][y]:
 				alive_neighbors = alive_neighbors + 1
 
 		if (y + 1) <= size:
-			if board[x][y + 1] == self.__ALIVE:
+			if self.board[x][y + 1]:
 				alive_neighbors = alive_neighbors + 1
 
 		if (y - 1) > 0:
-			if board[x][y - 1] == self.__ALIVE:
+			if self.board[x][y - 1]:
 				alive_neighbors = alive_neighbors + 1
-
 		#corners
-
 		if (x - 1) > 0 and (y - 1) > 0:
-			if board[x - 1][y - 1] == self.__ALIVE:
+			if self.board[x - 1][y - 1]:
 				alive_neighbors = alive_neighbors + 1
 
 		if (x + 1) <= size and (y - 1) > 0:
-			if board[x + 1][y - 1] == self.__ALIVE:
+			if self.board[x + 1][y - 1]:
 				alive_neighbors = alive_neighbors + 1
 
 		if(y + 1) <= size and (x - 1) > 0:
-			if board[x - 1][y + 1] == self.__ALIVE:
+			if self.board[x - 1][y + 1]:
 				alive_neighbors = alive_neighbors + 1
 
 		if(y + 1) <= size and (x + 1) <= size:
-			if board[x + 1][y + 1] == self.__ALIVE:
+			if self.board[x + 1][y + 1]:
 				alive_neighbors = alive_neighbors + 1
 
 		return alive_neighbors
 
-	"""Public functions - expose the API"""
-	#Get a first board randomly
-	def get_board(self , fill_random = False):
-		return [[self.__DEAD for col in range(self.width)] for row in range(self.heigth)]
+    #get a empty board
+	def __get_board(self):
+		return [[False for col in range(self.width)] for row in range(self.heigth)]
 
-	#This method aply the algorithm for update the life in panel
-	def update_life(self , board):
-		updated_board = [[self.__DEAD for col in range(self.width)] for row in range(self.heigth)]
-		for i , row in enumerate(board):
+	#Refresh board
+	def update_life(self):
+		updated_board = self.__get_board()
+		for i , row in enumerate(self.board):
 			for j , cell in enumerate(row):
-				number_of_neighbors = self.__get_alive_neighbors(i , j, board);
+				number_of_neighbors = self.__get_alive_neighbors(i , j);
 				updated_board[i][j] = self.__change_state(cell , number_of_neighbors)
-		return updated_board
+		self.board = updated_board
